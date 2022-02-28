b76e56a557ff7ffebe0577a864d805b1f5790e93
godud2604 <ees238@naver.com>
2022-02-28 17:49:28 +0900

    ♻️ Use according to conditions

--- js/main.js
+++ js/main.js
@@ -117,19 +117,19 @@
 
     switch (currentScene) {
       case 0:
-        const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
-        const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
-        const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
-        const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
-
-
         if (scrollRatio <= 0.22) {
           // in
+          const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
+          const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
+
           objs.messageA.style.opacity = messageA_opacity_in;
           objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
 
         } else {
           // out
+          const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
+          const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
+  
           objs.messageA.style.opacity = messageA_opacity_out;
           objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
 
